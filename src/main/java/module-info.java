59c07565e8c9f454d4bdbaaea2f114f944b7ed3c
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-02-25 00:23:08 +0100

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module pujalte.martinez.juan.chinook {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens pujalte.martinez.juan.chinook to javafx.fxml;
+    exports pujalte.martinez.juan.chinook;
+}
---
+++ src/main/java/pujalte/martinez/juan/chinook/HelloController.java
@@ -0,0 +1,14 @@
+package pujalte.martinez.juan.chinook;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
